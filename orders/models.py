8f0fd6f8243e37ad341605e4bd2e6440a54619fe
pedram <peeeedii@gmail.com>
2024-11-15 20:11:38 +0330

    fix: correct the typo

--- orders/models.py
+++ orders/models.py
@@ -32,7 +32,7 @@ class OrderItem(models.Model):
         on_delete=models.CASCADE
     )
     product = models.ForeignKey(
-        'shop.product', related_name='order_items', on_delete=models.CASCADE)
+        'shop.Product', related_name='order_items', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
