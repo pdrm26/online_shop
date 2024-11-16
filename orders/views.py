1c5d4e66827936fa3357f86531222c518cddc59c
pedram <peeeedii@gmail.com>
2024-11-17 00:21:22 +0330

    feat: handle async tasks with celery and their friends like rabbitmq and flower

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -20,6 +21,9 @@ def order_create(request):
 
             # clear the cart
             cart.clear()
+
+            # launch asynchronous send email task
+            order_created.delay(order.id)
             return render(request, "orders/order/created.html", {'order': order})
     else:
         form = OrderCreateForm()
