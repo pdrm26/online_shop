a9f68310068afed0d7cd075d053a59a15a0fb21d
pedram <peeeedii@gmail.com>
2024-11-20 22:19:23 +0330

    feat: add payment app to handle payment gateway

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
@@ -24,7 +24,10 @@ def order_create(request):
 
             # launch asynchronous send email task
             order_created.delay(order.id)
-            return render(request, "orders/order/created.html", {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            #redirect for payment
+            return redirect('payment:process')
     else:
         form = OrderCreateForm()
         return render(
