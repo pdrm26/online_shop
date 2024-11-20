8cb9ef9d36595a99e94df627396def1773760097
pedram <peeeedii@gmail.com>
2024-11-21 00:17:21 +0330

    feat: send the email with pdf when order paid

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.http import HttpResponse
 from orders.models import Order
+from .tasks import payment_completed
 
 
 @csrf_exempt
@@ -36,5 +37,7 @@ def stripe_webhook(request):
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+            # launch asynchronous task
+            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
