1ae29d2be2c197bd8a29939224fb64bc706674bc
pedram <peeeedii@gmail.com>
2024-11-22 00:15:36 +0330

    feat: add i18n with en and es

--- config/urls.py
+++ config/urls.py
@@ -2,15 +2,26 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
+from payment import webhooks
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
-    path('payment/', include('payment.urls', namespace='payment')),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
+    path(_('payment/'), include('payment.urls', namespace='payment')),
+    path(_('rosetta/'), include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
-]
+)
 
+urlpatterns += [
+    path(
+        'payment/webhook/',
+        webhooks.stripe_webhook,
+        name='stripe-webhook'
+    ),
+]
 
 if settings.DEBUG:
     urlpatterns += static(
